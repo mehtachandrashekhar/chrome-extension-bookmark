deba86fa053334f4c5fa85d999191391a1805cdf
mehtachandrashekhar <67406458+mehtachandrashekhar@users.noreply.github.com>
2023-06-24 12:07:24 +0530

    new commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function saveLead(){
+    console.log("Hello moto !")
+}
+
+let inputBtn = document.getElementById("input-btn");
+inputBtn.addEventListener("click",function() {
+    console.log('Buttom click from add event listener !')
+})
