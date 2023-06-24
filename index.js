8710a318525c1751e47dbadb52fc4e42e8dec752
mehtachandrashekhar <67406458+mehtachandrashekhar@users.noreply.github.com>
2023-06-24 19:57:21 +0530

    renderedn list items in ulEl

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-let myLead = [];
+let myLead = ["www.google.com","www.yahoo.com","www.wame.com"];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
-
+const ulEl = document.getElementById("ul-el");
 inputBtn.addEventListener("click",function() {
     myLead.push(inputEl.value)
-
-    console.log(myLead)
-    console.log("Hello moto !")
+    for(let i =0;i<myLead.length;i++){
+        ulEl.textContent = myLead[i]
+    }
 })
