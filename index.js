d543ac63185eb88d43927e50f1b93c8b245ac9e9
mehtachandrashekhar <67406458+mehtachandrashekhar@users.noreply.github.com>
2023-06-24 21:36:43 +0530

    working on render lead function

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-let myLead = ["www.google.com","www.yahoo.com","www.wame.com"];
-const inputEl = document.getElementById("input-el");
-const inputBtn = document.getElementById("input-btn");
-const ulEl = document.getElementById("ul-el");
+let myLead = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
 inputBtn.addEventListener("click",function() {
     myLead.push(inputEl.value)
-    for(let i =0;i<myLead.length;i++){
-        ulEl.textContent = myLead[i]
-    }
+    inputEl.value =''
+    renderLead()
+    
 })
+
+function renderLead(){
+    let listItems = ""
+    for(let i = 0; i<myLead.length; i++){
+        listItems += "<li>" + "<a>" + myLead[i] + "</a>" + "</li>"
+    }
+    ulEl.innerHTML = listItems    
+}
